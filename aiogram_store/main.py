108739c201aea98446ee1a197c89111d90882ebd
sergo <sergonatorgtr@gmail.com>
2021-04-17 08:02:42 +0300

    init 0.0.8

--- aiogram_store/main.py
+++ aiogram_store/main.py
@@ -1,33 +1,69 @@
 import os
+import shutil
 import sys
-from io import BytesIO, StringIO
 import requests
-from zipfile import ZipFile
 
 COMMANDS = ("install", "uninstall")
-STORE_JSON = "https://raw.githubusercontent.com/OGURCHINSKIY/aiogram_store/main/store.json"
 PACKAGES_LINK = "https://api.github.com/repos/OGURCHINSKIY/aiogram_store/contents/packages/{package}?ref=main"
+STORE_DIR = os.path.dirname(os.path.realpath(__file__))
+HELP = f"""aiogram store
+    usage:
+        install <package name> - install package
+        uninstall <package name> - uninstall package
+"""
+
+
+def create_path(filename: str):
+    if not os.path.exists(os.path.dirname(filename)):
+        try:
+            os.makedirs(os.path.dirname(filename))
+        except OSError:
+            return
+
+
+def download_dir(url: str):
+    with requests.get(url) as api:
+        for data in api.json():
+            if data.get("type") == "file":
+                create_path(os.path.join(STORE_DIR, data.get("path")))
+                with open(os.path.join(STORE_DIR, data.get("path")), "wb") as f:
+                    with requests.get(data.get("download_url")) as r:
+                        f.write(r.content)
+            elif data.get("type") == "dir":
+                download_dir(data.get("url"))
 
 
 def main():
     args = sys.argv[1:]
     if not args:
-        print("help")
+        print(HELP)
         sys.exit(0)
     if len(args) != 2:
-        print("wrong params count")
-        sys.exit(1)
+        print(HELP)
+        sys.exit(0)
     command, name = args
     if command not in COMMANDS:
-        print("what???")
-        sys.exit(1)
+        print(HELP)
+        sys.exit(0)
+    package_exist = os.path.exists(os.path.join(STORE_DIR, "packages", name))
     if command == "install":
-        package = requests.get(PACKAGES_LINK.format(package=name))
-        if package.status_code == 404:
-            print(f"package {name} not find")
+        if package_exist:
+            print("package already installed")
             sys.exit(0)
-        print(package.json())
-        cur_dir = os.path.dirname(os.path.realpath(__file__))
-        packages_path = os.path.join(cur_dir, "packages")
-        print(f"package {name} installed")
+        package_url = PACKAGES_LINK.format(package=name)
+        package = requests.get(package_url)
+        if package.status_code == 404:
+            print("package", name, "not found")
+            sys.exit(0)
+        print("loading package")
+        download_dir(package_url)
+        print("package", name, "installed")
+    elif command == "uninstall":
+        package = os.path.join(STORE_DIR, "packages", name)
+        if package_exist:
+            print("delete", name)
+            shutil.rmtree(package)
+            print(name, "deleted")
+        else:
+            print("package", name, "not found")
 
